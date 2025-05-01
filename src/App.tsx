dca1265fa937230d9ba21b4bf8585cdd6eaae847
JSebastianPaezSalesianoDev <159430211+JSebastianPaezSalesianoDev@users.noreply.github.com>
2025-05-01 22:14:51 +0100

    feat: Se hizo el componente Calendar, dentro la estructura basica html para tener una idea, y una estrucutra de event y el popup form basica para ir maquetando

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
+import Calendar from './components/calendar/calendar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,9 @@ function App() {
     <>
       <Login/>
       <Register/>
+      <div className="container">
+      <Calendar/>
+      </div>
     </>
   )
 }
