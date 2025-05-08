7e2793b16b19cf9d6ba208f26823ec3283d891ce
JSebastianPaezSalesianoDev <159430211+JSebastianPaezSalesianoDev@users.noreply.github.com>
2025-05-08 20:23:39 +0100

    Feat: Se agregaron los token a las peticiones

--- src/service/event.service.ts
+++ src/service/event.service.ts
@@ -1,42 +1,47 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8081/api/"
+const API_URL = "http://localhost:8081/api/";
 
-const aGetEventsById = async (userId: string /**jwt token o user */) => {
-    const response = await axios.get(API_URL + "event/" + userId + "/event", {
-       /*  headers: {
-            
-            'Authorization': `Bearer ${userId}` // jwt token o user
-
-        } */
-       auth: {
-            username: `Alexis`, // userId o email
-            password: '1234' //
-            // password o token
-        }
-    }); // quitar el 2do event luego
-    return response.data;
-}
-/* const aGetEventsById = async (token: string, userId: string) => {
-    const response = await axios.get(API_URL + "event/" + userId + "/event", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+const aAuthLogin = async (userId: string, password: string) => {
+    const response = await axios.post(API_URL + "auth/login", {
+        username: userId,
+        password: password
     });
     return response.data;
-  }; */
-const aCreateEvent = async (event: FormData) => {
-    const response = await axios.post(API_URL + "event", event);
-    return response.data;
-}
+};
 
-const aAllEvents = async () => {
-    const response = await axios.get(API_URL + "users/" + "event");
+const aGetEventsById = async (token: string, userId: string) => {
+    const response = await axios.get(`${API_URL}event/${userId}/event`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
     return response.data;
-}
+};
+
+const aCreateEvent = async (token: string, event: FormData) => {
+    const response = await axios.post(`${API_URL}event`, event, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    return response.data;
+};
+
+const aAllEvents = async (token: string) => {
+    const response = await axios.get(`${API_URL}users/event`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    return response.data;
+};
+
 const EventsService = {
+    aAuthLogin,
     aGetEventsById,
     aCreateEvent,
-    aAllEvents
-}
+    aAllEvents,
+};
+
 export default EventsService;
