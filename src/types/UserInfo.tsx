46f35404529700240544d7f77f69aee19fd66f4c
JSebastianPaezSalesianoDev <159430211+JSebastianPaezSalesianoDev@users.noreply.github.com>
2025-05-21 19:03:28 +0100

    ReFact: Se refactorizo el type UserInfo para obtener el role, y ahora en un useEffect para ver cambios en tiempo real

--- src/types/UserInfo.tsx
+++ src/types/UserInfo.tsx
@@ -1,5 +1,28 @@
-export const Userinfo = {
-    token: localStorage.getItem("authToken"),
-    userId: localStorage.getItem("userId"),
-    username: localStorage.getItem("username")
-}
+import React, { useEffect, useState } from "react";
+import EventsService from "../service/event.service";
+
+export const useUserInfo = () => {
+    const [userInfo, setUserInfo] = useState({
+        token: localStorage.getItem("authToken"),
+        userId: localStorage.getItem("userId"),
+        username: localStorage.getItem("username")
+    });
+
+    const [userRole, setUserRole] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (userInfo.token && userInfo.userId) {
+            const fetchUserRole = async () => {
+                try {
+                    const role = await EventsService.aGetUserById(userInfo.token!, userInfo.userId!);
+                    setUserRole(role);
+                } catch (error) {
+                    console.error("Error fetching user role:", error);
+                }
+            };
+            fetchUserRole();
+        }
+    }, [userInfo.token, userInfo.userId]);
+
+    return { userInfo, userRole };
+};
